d2285c2f8650bd0464c71a063c2ef2b7da1bfd1d
samantha vieira <105881146+samanthavf@users.noreply.github.com>
2024-12-13 21:06:32 -0300

    first-commit

---
+++ spacefood-back/src/main/java/com/samantha/spacefood_back/SpacefoodBackApplication.java
@@ -0,0 +1,13 @@
+package com.samantha.spacefood_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacefoodBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacefoodBackApplication.class, args);
+	}
+
+}
---
+++ spacefood-back/src/test/java/com/samantha/spacefood_back/SpacefoodBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samantha.spacefood_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacefoodBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
