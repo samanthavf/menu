37553197038471c233eb1dedad482c146730e367
samantha vieira <105881146+samanthavf@users.noreply.github.com>
2024-12-16 15:53:32 -0300

    services classes commit

---
+++ spacefood-back/src/main/java/com/samantha/spacefood_back/services/DishService.java
@@ -0,0 +1,11 @@
+package com.samantha.spacefood_back.services;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class DishService {
+
+}
