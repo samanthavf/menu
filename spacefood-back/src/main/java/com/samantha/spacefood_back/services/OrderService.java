d7a68700fd8d2422a81ef9b06b9e8456f23c65ac
samantha vieira <105881146+samanthavf@users.noreply.github.com>
2024-12-17 15:04:28 -0300

    orderService commit

--- spacefood-back/src/main/java/com/samantha/spacefood_back/services/OrderService.java
+++ spacefood-back/src/main/java/com/samantha/spacefood_back/services/OrderService.java
@@ -1,7 +1,11 @@
 package com.samantha.spacefood_back.services;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.samantha.spacefood_back.dtos.OrderDTO;
+import com.samantha.spacefood_back.entities.Order;
 import com.samantha.spacefood_back.repositories.OrderRepo;
 
 import lombok.RequiredArgsConstructor;
@@ -12,8 +16,22 @@ public class OrderService {
 	private final OrderRepo orderRepo;
 	
 	//CRIAR PEDIDO
+	public Order createOrder(OrderDTO dto) {
+		Order pedido = new Order();
+		pedido.setPratos(dto.pratos());
+		pedido.setValorTotal(dto.valortotal());
+		pedido.setNumeroMesa(dto.numeromesa());
+		return orderRepo.save(pedido);
+	}
 	
 	//LISTAR PEDIDOS
+	public Page<Order> readOrders(Pageable pageable){
+		return orderRepo.findAll(pageable);
+	}
 	
 	//CANCELAR PEDIDO
+	public void cancelOrder(Long id) {
+        orderRepo.deleteById(id);
+	}
+	
 }
