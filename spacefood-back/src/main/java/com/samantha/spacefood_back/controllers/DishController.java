d300feb8431610e469888f3e1875b563752f3dff
samantha vieira <105881146+samanthavf@users.noreply.github.com>
2024-12-17 17:03:46 -0300

    dishcontroller commit

--- spacefood-back/src/main/java/com/samantha/spacefood_back/controllers/DishController.java
+++ spacefood-back/src/main/java/com/samantha/spacefood_back/controllers/DishController.java
@@ -1,10 +1,22 @@
 package com.samantha.spacefood_back.controllers;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.samantha.spacefood_back.dtos.DishDTO;
+import com.samantha.spacefood_back.entities.Dish;
 import com.samantha.spacefood_back.services.DishService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -13,4 +25,28 @@ import lombok.RequiredArgsConstructor;
 public class DishController {
 	private final DishService service;
 	
+	//CREATE
+	@GetMapping("/adicionar")
+	public ResponseEntity<Dish> create(@RequestBody @Valid DishDTO dto){
+		return new ResponseEntity<>(service.createDish(dto), HttpStatus.CREATED);
+	}
+	//READ
+	@GetMapping("/listar")
+	public ResponseEntity<Page<Dish>> read (Pageable pageable){
+		return ResponseEntity.ok(service.readDishes(pageable));
+	}
+	
+	//REMOVE
+	@DeleteMapping(path = "/remover/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+	}
+	
+	//UPDATE
+	@PutMapping("/atualizar")
+	public ResponseEntity<Void> update(@RequestBody @Valid DishDTO dto) throws Exception{
+		service.updateDish(dto);
+		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+	}
 }
--- spacefood-back/src/main/java/com/samantha/spacefood_back/controllers/OrderController.java
+++ spacefood-back/src/main/java/com/samantha/spacefood_back/controllers/OrderController.java
@@ -4,6 +4,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,16 +25,19 @@ public class OrderController {
 	private final OrderService service;
 	
 	//Create
+	@GetMapping("/pedir")
 	public ResponseEntity<Order> create(@RequestBody @Valid OrderDTO dto){
 		return new ResponseEntity<>(service.createOrder(dto), HttpStatus.CREATED);
 	}
 	
 	//READ
+	@GetMapping("/listar")
 	public ResponseEntity<Page<Order>> read(Pageable pageable){
 		return ResponseEntity.ok(service.readOrders(pageable));
 	}
 	
 	//DELETE
+	@DeleteMapping(path = "/remover/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.cancelOrder(id);
 		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
